bdc5ecea4c637f195457ed9d95784c92d3381974
kinugasaemi <55238525+kinugasaemi@users.noreply.github.com>
2019-10-27 16:27:02 +0900

    Revert "Revert "メッセージ送信機能実装""

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,36 @@
 class MessagesController < ApplicationController
+  #このページ全てにset_groupメソッドを適用。
+  before_action :set_group
+
+  #messages/indexページでメッセージが作られる。
+  #また、@group.messagesも定義。
   def index
-    @group = current_user.groups
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
+  #messages/createページでメッセージが保存されると、group_messagesにリダイレクト。
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    #messages/createページでメッセージが保存失敗すると、’メッセージを入力してください。’と表示。
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください！'
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  #現在グループIDをparamsで取得。
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
