03861797b3592e845dc8c5d72a262841d6bd773e
kinugasaemi <55238525+kinugasaemi@users.noreply.github.com>
2019-10-27 14:21:15 +0900

    Revert "メッセージ送信機能実装"

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,44 +1,41 @@
 class GroupsController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  #groupのnewページでグループ情報を登録。
-  def new
-    @group = Group.new
-    @group.users << current_user
-  end
+	def new
+		@group = Group.new
+		@group.users << current_user
+	end
 
-  #groupのnewベージでグループ作成完了。
-  def create
-    @group = Group.new(group_params)
-    if @group.save
-      redirect_to root_path,notice: 'グループを作成しました'
-    else
-      flash[:alert] = '保存に失敗しました'
-      render :new
-    end
-  end
+	def create
+		@group = Group.new(group_params)
+		if @group.save
+			redirect_to root_path,notice: 'グループを作成しました'
+		else
+			flash[:alert] = '保存に失敗しました'
+			render :new
+		end
+	end
 
-  #groupのupdateページでグループ編集。編集されたgroup_messagesのpathへ。
-  def update
-    if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
-  end
+	def update
+		if @group.update(group_params)
+			redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+		else
+			render :edit
+		end
+	end
 
-  private
-  #現在ログイン中のユーザIDをparamsで取得。
-  def group_params
-    params.require(:group).permit(:name, user_ids: [] )
-  end
+	private
+	def group_params
+		params.require(:group).permit(:name, { :user_ids => []})
+	end
 
-  #現在グループIDをparamsで取得。
-  def set_group
-    @group = Group.find(params[:id])
-  end
+	def set_group
+		@group = Group.find(params[:id])
+	end
 
-  def edit
-  end
+	def edit
+	end
+
+
 end
