3137a339218d37caaad16ff8e2d9c45dc22cfd2b
kinugasaemi <dsrxd448@yahoo.co.jp>
2019-10-27 17:31:59 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
@@ -17,5 +16,7 @@ module ChatSpace
       g.mailer false
     end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+
   end
 end
